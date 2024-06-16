bcfcb250f7e1d0e038b46e14f2df2eeab1b4d7d3
Danamaz <danayebidazeph@gmail.com>
2024-06-16 23:28:48 +0000

    Creating a new dart file for the splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 //import 'package:final_project/firebase_options.dart';
-import 'package:final_project/screens/loginpage.dart';
-import 'dart:async';
+import 'package:final_project/screens/splashscreen';
 import 'package:flutter/material.dart';
 //import 'package:firebase_core/firebase_core.dart';
 //import 'package:get_storage/get_storage.dart';
@@ -37,40 +36,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const SplashScreen(),
     );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const LoginPage()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.indigo,
-        body: Center(
-          child: Text(
-            'Splash Screen',
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.bold),
-          ),
-        ));
   }
 }
